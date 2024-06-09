a681462dc1019bda7cefb6a3ef87549df0f47206
onsato <nogiglee@gmail.com>
2024-06-09 18:41:59 +0900

    初めてのコンポーネント

---
+++ my-gatsby-site/src/pages/basic/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Profile from "./profile"
+
+const Basic = () => {
+    return (
+        <>
+             <Profile />
+        </>
+    )
+}
+
+export default Basic
+
---
+++ my-gatsby-site/src/pages/basic/profile.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Profile = () => {
+    return (
+        <img
+      src="https://i.imgur.com/lICfvbD.jpg"
+      alt="Aklilu Lemma"
+    />
+    )
+}
+
+export default Profile
